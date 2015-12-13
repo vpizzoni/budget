5dc07771b652f1df0c38d8012299c0f81b3a037f
Dr Valentim Rodrigues  Salgado Vieira Pizzoni <valentim.pizzoni@gmail.com>
2015-12-13 17:28:32 -0200

    Initial Commit

---
+++ budget/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  budget
+//
+//  Created by Valentim Pizzoni on 13/12/15.
+//  Copyright © 2015 Valentim Pizzoni. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ budget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  budget
+//
+//  Created by Valentim Pizzoni on 13/12/15.
+//  Copyright © 2015 Valentim Pizzoni. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
